429704648474159503d9e51b543839f2c3a6f926
Huy Nguyen <qhuydtvt@gmail.com>
2016-05-13 20:33:51 +0700

    Session7

---
+++ Session7/src/vn/edu/techkids/Main.java
@@ -0,0 +1,27 @@
+package vn.edu.techkids;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        File soundFile = new File("resources/arrow_x.wav");
+//        try {
+//            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+//            Clip clip = AudioSystem.getClip();
+//            clip.open(audioIn);
+//            clip.start();
+//            clip.loop(Clip.LOOP_CONTINUOUSLY);
+//        } catch (UnsupportedAudioFileException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (LineUnavailableException e) {
+//            e.printStackTrace();
+//        }
+        GameWindow gameWindow = new GameWindow();
+    }
+}
